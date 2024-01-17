01464a598ecb62316c28591fea897b8627b5bf55
cchristion <47944263+cchristion@users.noreply.github.com>
2024-01-17 14:19:51 +0530

    Fixed day/part

--- day01/day01_part2.v1.py
+++ day01/day01_part2.v1.py
@@ -1,7 +1,7 @@
 spelled = {0:"zero", 1: "one", 2: "two", 3: "three", 4: "four", 
            5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
 
-with open("test.txt") as file:
+with open("input_part2.txt") as file:
     file = file.readlines()
 
 file = [x.strip() for x in file]
